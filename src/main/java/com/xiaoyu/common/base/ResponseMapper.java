8aa01210144d57765fbd0f494d0820ba1037d6f2
xiaoyu <1546428286@qq.com>
2017-03-16 17:23:59 +0800

    修改mapper 改为单例模式

--- src/main/java/com/xiaoyu/common/base/ResponseMapper.java
+++ src/main/java/com/xiaoyu/common/base/ResponseMapper.java
@@ -1,79 +1,100 @@
 package com.xiaoyu.common.base;
 
+import java.util.Map;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.serializer.SerializerFeature;
 import com.alibaba.fastjson.serializer.ValueFilter;
 import com.google.common.collect.Maps;
-import java.util.Map;
 
-/**返回結果的封裝
+/**
+ * 2017年3月16日下午5:22:13
+ * 
  * @author xiaoyu
- *
+ * @description 返回数据的封装
+ * @version 2.0
  */
 public class ResponseMapper {
+	/**
+	 * 默认为成功
+	 */
 	private String code = ResultConstant.SUCCESS;
-	private String message = null;
-	private Long count;
-	private Object datas;
-	/* 解決fastjson null轉為"" */
+
 	private final ValueFilter filter = new ValueFilter() {
 		@Override
-		public Object process(Object arg0, String arg1, Object value) {
-			if (null == value)
+		public Object process(Object object, String name, Object value) {
+			if (null == value || "null".equals(value))
 				return "";
 			return value;
 		}
 	};
 
-	public String getResultJson() {
-		Map<String, Object> map = Maps.newHashMap();
-		map.put("code", this.code);
-		switch (this.code) {
-		case ResultConstant.ARGS_ERROR:
-			map.put("message", ResultConstant.ARGS_ERROR_MESSAGE);
-			break;
-		case ResultConstant.EXCEPTION:
-			map.put("message", ResultConstant.EXCEPTION_MESSAGE);
-			break;
-		case ResultConstant.EXISTS:
-			map.put("message", ResultConstant.EXISTS_MESSAGE);
-			break;
-		case ResultConstant.NOT_DATA:
-			map.put("message", ResultConstant.NOT_DATA_MESSAGE);
-			break;
-		default:
-			map.put("message", ResultConstant.SUCCESS_MESSAGE);
-		}
-		if (message != null)
-			map.put("message", this.message);
-		if (this.count != null) {
-			map.put("count", this.count);
-		}
-		if (this.datas != null) {
-			map.put("datas", this.datas);
-		}
+	/**
+	 * 封装响应的数据
+	 */
+	private Map<String, Object> dataMap = Maps.newHashMap();
 
-		return JSON.toJSONString(map, filter, SerializerFeature.EMPTY);
+	private ResponseMapper() {
+		initMap();
+	};
+
+	/**
+	 * 单例
+	 */
+	private static class MapperInstance {
+		public static ResponseMapper mapper = new ResponseMapper();
+	}
+
+	public static ResponseMapper createMapper() {
+		return MapperInstance.mapper;
+	}
+
+	private void initMap() {
+		dataMap.put("code", code);
+		dataMap.put("message", ResultConstant.SUCCESS_MESSAGE);
+		dataMap.put("count", null);
+		dataMap.put("datas", null);
+	}
+
+	public String getResultJson() {
+		String json = JSON.toJSONString(dataMap, filter, SerializerFeature.EMPTY);
+		this.initMap();// 重置map
+		return json;
 	}
 
 	public ResponseMapper setCode(String code) {
-		this.code = code;
+		dataMap.put("code", code);
+		switch (code) {
+		case ResultConstant.ARGS_ERROR:
+			dataMap.put("message", ResultConstant.ARGS_ERROR_MESSAGE);
+			break;
+		case ResultConstant.EXCEPTION:
+			dataMap.put("message", ResultConstant.EXCEPTION_MESSAGE);
+			break;
+		case ResultConstant.EXISTS:
+			dataMap.put("message", ResultConstant.EXISTS_MESSAGE);
+			break;
+		case ResultConstant.NOT_DATA:
+			dataMap.put("message", ResultConstant.NOT_DATA_MESSAGE);
+			break;
+		}
 		return this;
 	}
 
 	public ResponseMapper setMessage(String message) {
-		this.message = message;
+		if (message != null)
+			dataMap.put("message", message);
 		return this;
 	}
 
 	public ResponseMapper setCount(Long count) {
-		this.count = count;
+		if (count != null)
+			dataMap.put("count", count);
 		return this;
 	}
 
 	public ResponseMapper setDatas(Object datas) {
-		this.datas = datas;
+		if (datas != null)
+			dataMap.put("datas", datas);
 		return this;
 	}
-
 }
